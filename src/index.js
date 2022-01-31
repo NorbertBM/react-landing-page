c3d25589650cb93c1340ec07ed62221d218c4cb2
Norbert Menyhart - Anton Steininger GmbH <menyhart.b.norbert@gmail.com>
2022-01-31 15:23:14 +0100

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Services from "./routs/services";
+import Header from "./routs/header";
+import Contact from "./routs/contact";
+import About from "./routs/about";
+import LogIn from "./routs/logIn";
+import SignUp from "./routs/signUp";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="header" element={<Header />} />
+        <Route path="services" element={<Services />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="logIn" element={<LogIn />} />
+        <Route path="signUp" element={<SignUp />} />
+      </Route>
+    </Routes>
+  </Router>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById("root")
+);
